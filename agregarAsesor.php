520f50d3ab4e0f2128c1c338d57bba9c26dc2665
Yonathan Martinez <33378318+MartinezYonathan@users.noreply.github.com>
2017-11-26 19:16:11 -0600

    Add files via upload

---
+++ agregarAsesor.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<!--
+	TXT by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>SAT-UACM</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="css/styleMain.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+			<div id="header-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="12u">
+
+							<header id="header">
+								<h1><a href="index.php" id="logo">SAT-UACM</a></h1>
+								<nav id="nav">
+									<a href="index.php">inicio</a>
+									<a href="agregarCandi.php">AGREGAR CANDIDATO</a>
+									<a href="agregarAsesor.php"
+									class="current-page-item">
+									 AGREGAR ASESOR</a>
+									<a href="valCandidato.php">VALIDAR CANDIDATO</a>
+									<a href="sminarios.php">SEMINARIOS</a>
+								</nav>
+							</header>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="main">
+			<div class="container">
+					<div class="row main-row">
+						<div class="12u">
+
+							<section>
+								<h2>Agregar Asesor</h2>
+								
+							</section>
+
+						</div>
+					</div>
+			</div>
+	</div>
+
+	<div class="12u">
+		<div id="copyright">
+			&copy; UACM. Todos los derechos reservados. | Diseo: <a href="#">MABYCODE</a>
+		</div>
+	</div>
+
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+<!--
+	TXT by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>SAT-UACM</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="css/styleMain.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		
+			<div id="header-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="12u">
+
+							<header id="header">
+								<h1><a href="index.php" id="logo">SAT-UACM</a></h1>
+								<nav id="nav">
+									<a href="index.php" class="current-page-item">INICIO</a>
+									<a href="agregarCandi.php">AGREGAR CANDIDATO</a>
+									<a href="agregarAsesor.php">AGREGAR ASESOR</a>
+									<a href="valCandidato.php">VALIDAR CANDIDATO</a>
+									<a href="sminarios.php">SEMINARIOS</a>
+								</nav>
+							</header>
+
+						</div>
+					</div>
+				</div>
+			
+
+
+	<div id="banner-wrapper">
+			<div class="container">
+
+				<div id="banner">
+					<h2>Seminario de Ayuda a Titulacion</h2>
+					<span>Nada Humano Me Es Ajeno ...</span>
+				</div>
+			</div>
+	</div>
+
+	<div id="main">
+			<div class="container">
+					<div class="row main-row">
+						<div class="12u">
+							<section>
+								<h2>Ayuda a estudiuantes de tesis</h2>
+								<p>This is the second generic imperdiet suscipit.</p>
+								<p>Maurisporta ut semper quam iaculis.</p>
+								<p>Nulla ut tortor quam, a luctus i.</p>
+							</section>
+
+							<section>
+								<center><div class="tit"><h2 style="color: #0000FF; ">Inicio de sesión</h2>
+								<center><div class="Ingreso">
+
+								<table border="0" align="center" valign="middle">
+									<tr>
+									<td rowspan=2>
+										<form action="validar.php" method="post">
+											<table border="0">
+												<tr><td><label style="font-size: 14pt"><b>Correo: </b></label></td>
+													<td width=80> <input class="form-group has-success" style="border-radius:15px;" type="text" name="mail"></td></tr>
+												<tr><td><label style="font-size: 14pt"><b>Contraseña: </b></label></td>
+													<td witdh=80><input style="border-radius:15px;" type="password" name="pass"></td></tr>
+												<tr><td></td>
+													<td width=80 align=center><input class="btn btn-primary" type="submit" value="Aceptar"></td>
+												</tr></tr></table>
+										</form>
+									<br>
+							</section>
+
+						</div>
+					</div>
+			</div>
+	</div>
+
+	<div class="12u">
+		<div id="copyright">
+			&copy; UACM. Todos los derechos reservados. | Diseo: <a href="#">MABYCODE</a>
+		</div>
+	</div>
+
+
+	</body>
+
+</html>
