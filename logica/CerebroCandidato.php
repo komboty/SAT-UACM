4c8ddd4e07728d5084ff12ee56770bfb4ecead4d
Javier Monroy Salcedo <armoniadelaberintos@gmail.com>
2017-12-02 22:26:26 -0600

    pantalla validar candidatos terminada con script separado del doc html con autor

--- logica/CerebroCandidato.php
+++ logica/CerebroCandidato.php
@@ -4,6 +4,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Creado por Javier Monroy Salcedo
  */
 include '../Persistencia/DBConnection.php';
 //include '../Persistencia/base/candidatoDBHelper.php';
